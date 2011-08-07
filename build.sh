54be8ca7d2d617436b87ad49a4bf15e69aee511a
okay <okay.zed@gmail.com>
2011-08-06 21:03:28 -0700

    Add transformer to build data.js

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 # usage: build.sh data/some-file.tcp
 
+# Do normal processing
 cat $1 | python process-dump.py > packet_flight/data.pde
+
+# Setup javascript viewer
+cp packet_flight/data.pde packet_flight/js/data.js
+
+# Clean out the junk
+sed -i 's/NetworkNode \(.*\) =/\1 =/g' packet_flight/data.pde packet_flight/js/data.js
+sed -i 's/NetworkNode node;.*$//' packet_flight/data.pde packet_flight/js/data.js
+sed -i 's/void/function/g' packet_flight/data.pde packet_flight/js/data.js
+
