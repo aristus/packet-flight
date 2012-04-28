695a605df75e0e31d103b9d0caf94724f27ee115
okay <okay@okay-x201.(none)>
2012-04-28 09:22:13 -0700

    Update README

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ sed -i 's/NetworkNode \(.*\) =/\1 =/g' packet_flight/data.pde packet_flight/js/d
 sed -i 's/NetworkNode node;.*$//' packet_flight/data.pde packet_flight/js/data.js
 sed -i 's/void/function/g' packet_flight/data.pde packet_flight/js/data.js
 
+# to view the results, open packet_flight/viewer.html or
+# load packet_flight/data.pde in processing
